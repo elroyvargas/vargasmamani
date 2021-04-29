1250609830a16ae28920106d2f0488382c968153
elroyvargas <>
2021-04-29 01:23:21 +0000

    hola

---
+++ EstCondicional.py
@@ -0,0 +1,31 @@
+def estCondicional01():
+  #Definir variables y otros
+  print("Ejemplo estructura Condicional en Python")
+  montoP=0
+  #Datos de entrada
+  cantidadX=int(input("Ingrese la cantidad de lapices:"))
+  #Proceso
+  if cantidadX>=1000:
+    montoP=cantidadX*0.80
+  else:
+    montoP=cantidadX*0.90
+  #Datos de salida
+  print("El monto a pagar es:", montoP)
+#estCondicional01()
+def estCondicional02():
+  #Definir variables y otros
+  print("Ejercicio 02 Est. Condicional")
+  montoP=0
+  #Datos de entrada
+  cantidadX=int(input("Ingrese la cantidad personas invitadas:"))
+  #Proceso
+  if cantidadX<200:
+    montoP=cantidadX*95
+  elif cantidadX>200 and cantidadX<=300:
+    montoP=cantidadX*85
+  else:
+    montoP=cantidadX*75
+  #Datos de salida
+  print("La cantidad a pagar es:", montoP)
+#------
+estCondicional02()
